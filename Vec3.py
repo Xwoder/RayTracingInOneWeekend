11c19cb076bfe93966060f2bcf85687d68b9f254
Xwoder <Xwoder@qq.com>
2026-09-15 13:57:35 +0800

    refactor(Vec3): 将分量访问改为只读属性
    移除原地赋值运算符，简化分量访问方式为属性获取。

--- Vec3.py
+++ Vec3.py
@@ -50,27 +50,30 @@ class Vec3:
 
         return cls(x, y, z)
 
+    @property
     def x(self) -> Number:
         """
-        获取向量的 x 分量。
+        向量的 x 分量（只读属性）。
 
         Returns:
             Number: x 轴方向的分量
         """
         return self._x
 
+    @property
     def y(self) -> Number:
         """
-        获取向量的 y 分量。
+        向量的 y 分量（只读属性）。
 
         Returns:
             Number: y 轴方向的分量
         """
         return self._y
 
+    @property
     def z(self) -> Number:
         """
-        获取向量的 z 分量。
+        向量的 z 分量（只读属性）。
 
         Returns:
             Number: z 轴方向的分量
@@ -109,82 +112,6 @@ class Vec3:
             return self._z
         raise IndexError("Vec3 index out of range")
 
-    def __iadd__(self, other: "Vec3") -> "Vec3":
-        """
-        原地加法赋值运算符（v += u），将自身各分量加上 other 的对应分量，
-        并返回自身（对应 C++ 的 vec3& operator+=(const vec3& v)）。
-
-        Args:
-            other (Vec3): 加到自身的另一个向量。
-
-        Returns:
-            Vec3: 自身（已就地修改）
-        """
-        self._x += other.x()
-        self._y += other.y()
-        self._z += other.z()
-        return self
-
-    def __isub__(self, other: "Vec3") -> "Vec3":
-        """
-        原地减法赋值运算符（v -= u），将自身各分量减去 other 的对应分量，
-        并返回自身（对应 C++ 的 vec3& operator-=(const vec3& v)）。
-
-        Args:
-            other (Vec3): 被减的向量。
-
-        Returns:
-            Vec3: 自身（已就地修改）
-        """
-        self._x -= other.x()
-        self._y -= other.y()
-        self._z -= other.z()
-        return self
-
-    def __imul__(self, other: "Vec3 | Number") -> "Vec3":
-        """
-        原地乘法赋值运算符（v *= t 或 v *= u），就地修改自身各分量，
-        并返回自身（对应 C++ 的 vec3& operator*=(double t)）。
-        - 标量 t：各分量乘以 t。
-        - 向量 u：逐分量相乘（Hadamard 积）。
-
-        Args:
-            other (Vec3 | Number): 右侧操作数，可为标量或向量。
-
-        Returns:
-            Vec3: 自身（已就地修改）
-        """
-        if isinstance(other, Vec3):
-            self._x *= other.x()
-            self._y *= other.y()
-            self._z *= other.z()
-        else:
-            self._x *= other
-            self._y *= other
-            self._z *= other
-        return self
-
-    def __itruediv__(self, other: "Vec3 | Number") -> "Vec3":
-        """
-        原地除法赋值运算符（v /= t 或 v /= u），就地修改自身并返回自身。
-        对应 C++ 的 vec3& operator/=(double t)，其实现为 *this *= 1/t。
-        - 标量 t：委托给 __imul__(1/t)（即乘以倒数）。
-        - 向量 u：逐分量相除。
-
-        Args:
-            other (Vec3 | Number): 右侧操作数，可为标量或向量。
-
-        Returns:
-            Vec3: 自身（已就地修改）
-        """
-        if isinstance(other, Vec3):
-            self._x /= other.x()
-            self._y /= other.y()
-            self._z /= other.z()
-        else:
-            self.__imul__(1 / other)
-        return self
-
     def __add__(self, other: "Vec3") -> "Vec3":
         """
         向量加法（v + u），返回新向量（对应 C++ 的 operator+）。
@@ -196,7 +123,7 @@ class Vec3:
         Returns:
             Vec3: 分量相加后的新向量
         """
-        return Vec3(self._x + other.x(), self._y + other.y(), self._z + other.z())
+        return Vec3(self._x + other.x, self._y + other.y, self._z + other.z)
 
     def __sub__(self, other: "Vec3") -> "Vec3":
         """
@@ -209,7 +136,7 @@ class Vec3:
         Returns:
             Vec3: 分量相减后的新向量
         """
-        return Vec3(self._x - other.x(), self._y - other.y(), self._z - other.z())
+        return Vec3(self._x - other.x, self._y - other.y, self._z - other.z)
 
     def __mul__(self, other: "Vec3 | Number") -> "Vec3":
         """
@@ -224,7 +151,7 @@ class Vec3:
             Vec3: 相乘后的新向量
         """
         if isinstance(other, Vec3):
-            return Vec3(self._x * other.x(), self._y * other.y(), self._z * other.z())
+            return Vec3(self._x * other.x, self._y * other.y, self._z * other.z)
         return Vec3(self._x * other, self._y * other, self._z * other)
 
     def __rmul__(self, other: "Number") -> "Vec3":
@@ -252,7 +179,7 @@ class Vec3:
             Vec3: 相除后的新向量
         """
         if isinstance(other, Vec3):
-            return Vec3(self._x / other.x(), self._y / other.y(), self._z / other.z())
+            return Vec3(self._x / other.x, self._y / other.y, self._z / other.z)
         return Vec3(self._x / other, self._y / other, self._z / other)
 
     def __eq__(self, other: object) -> bool:
@@ -269,7 +196,7 @@ class Vec3:
         """
         if not isinstance(other, Vec3):
             return NotImplemented
-        return self._x == other.x() and self._y == other.y() and self._z == other.z()
+        return self._x == other.x and self._y == other.y and self._z == other.z
 
     def length_squared(self) -> float:
         """
@@ -302,7 +229,7 @@ class Vec3:
         Returns:
             Number: 各分量乘积之和（x1*x2 + y1*y2 + z1*z2）
         """
-        return self._x * other.x() + self._y * other.y() + self._z * other.z()
+        return self._x * other.x + self._y * other.y + self._z * other.z
 
     def normalized(self) -> "Vec3":
         """
@@ -340,9 +267,9 @@ class Vec3:
             Vec3: 叉积得到的新向量
         """
         return Vec3(
-            self._y * other.z() - self._z * other.y(),
-            self._z * other.x() - self._x * other.z(),
-            self._x * other.y() - self._y * other.x(),
+            self._y * other.z - self._z * other.y,
+            self._z * other.x - self._x * other.z,
+            self._x * other.y - self._y * other.x,
         )
 
 
@@ -354,8 +281,8 @@ if __name__ == '__main__':
     s1 = Vec3.from_sequence([1, 2, 3])
     s2 = Vec3.from_sequence((1, 2, 3))
 
-    print(v.x(), v.y(), v.z())
+    print(v.x, v.y, v.z)
     print(f"{v.length() = }")
 
-    print(s1.x(), s1.y(), s1.z())
-    print(s2.x(), s2.y(), s2.z())
+    print(s1.x, s1.y, s1.z)
+    print(s2.x, s2.y, s2.z)
