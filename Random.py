0cebf16d14bb5fb6cf98c96c0b4cee42c313953c
Xwoder <Xwoder@qq.com>
2026-09-16 22:13:49 +0800

    feat: 新增随机数生成模块

---
+++ Random.py
@@ -0,0 +1,13 @@
+import numpy as np
+from numpy.random import Generator
+
+from Number import Number
+
+_rng: Generator = np.random.Generator(np.random.MT19937())
+
+
+def random_number(
+        min: float = 0.0,
+        max: float = 1.0) -> Number:
+    """Returns a random real in [min, max)."""
+    return float(min + (max - min) * _rng.random())
