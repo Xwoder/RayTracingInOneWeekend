ea93284823a32b7d0d0c1c666698abfaf1e89731
Xwoder <Xwoder@qq.com>
2026-09-18 10:48:08 +0800

    refactor(Random): 将 min/max 参数类型标注改为 Number

--- Random.py
+++ Random.py
@@ -7,7 +7,7 @@ _rng: Generator = np.random.Generator(np.random.MT19937())
 
 
 def random_number(
-        min: float = 0.0,
-        max: float = 1.0) -> Number:
+        min: Number = 0.0,
+        max: Number = 1.0) -> Number:
     """Returns a random real in [min, max)."""
     return float(min + (max - min) * _rng.random())
